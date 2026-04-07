c1a2ad7e624606478556503b1861cf57b100d1b4
hug0loE6 <hugo.lopez.20036@gmail.com>
2026-04-07 19:15:37 +0200

    boutton qui bouttone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
--- app/src/main/java/com/example/traveling/TravelpathProperties.java
+++ app/src/main/java/com/example/traveling/TravelpathProperties.java
@@ -1,6 +1,8 @@
 package com.example.traveling;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class TravelpathProperties extends BottomSheetDialogFragment {
-
 
         @Nullable
         @Override
@@ -22,10 +25,37 @@ public class TravelpathProperties extends BottomSheetDialogFragment {
 
             View view = inflater.inflate(R.layout.travelpath_properties, container, false);
 
+
+
             Button confirme = view.findViewById(R.id.confirm);
+            ChipGroup chipAct = view.findViewById(R.id.chipAct);
+
             confirme.setOnClickListener(v ->{
                 dismiss();
             });
+
+            String[] activites = {
+                    "Restauration", "Loisirs", "Culture"
+            };
+
+
+            for (String act : activites) {
+                Chip chip = new Chip(requireContext());
+                chip.setText(act);
+                chip.setCheckable(true);
+                chip.setChecked(false);
+                chip.setChipBackgroundColorResource(R.color.white);
+                chip.setOnClickListener(v -> {
+                    chip.setSelected(!chip.isSelected());
+                    if(chip.isSelected()){
+                        chip.setChipBackgroundColorResource(R.color.greenSelec);
+                    } else {
+                        chip.setChipBackgroundColorResource(R.color.white);
+                    }
+                });
+                chipAct.addView(chip);
+
+            }
 
             return view;
         }
